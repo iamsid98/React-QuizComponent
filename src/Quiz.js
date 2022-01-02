8eba5aa9898cafb73e6cec90f6910cb0c4b10881
iamsid98 <96907358+iamsid98@users.noreply.github.com>
2022-01-02 21:10:07 +0530

    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,9 @@ class Quiz extends Component {
       quiz_position: 1
     };
   }
+  showNextQuestion() {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -22,6 +25,7 @@ class Quiz extends Component {
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
           />
         )}
       </div>
