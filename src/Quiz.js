d836fd2a527db9ef1b18294b338f6de0d509606a
iamsid98 <96907358+iamsid98@users.noreply.github.com>
2022-01-02 10:10:03 +0530

    Creating a QuizEnd Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import QuizEnd from "./QuizEnd";
 import QuizQuestion from "./QuizQuestion";
 let quizData = require("./quiz_data.json");
 
@@ -10,11 +11,19 @@ class Quiz extends Component {
     };
   }
   render() {
+    const isQuizEnd =
+      quizData.quiz_questions.length === this.state.quiz_position - 1;
     return (
       <div>
-        <QuizQuestion
-          quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-        />
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
       </div>
     );
   }
